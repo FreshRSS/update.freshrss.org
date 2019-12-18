557e57189c3ab558d2483ddd7b62fd17f5d9cc0d
Marien Fressinaud <dev@marienfressinaud.fr>
2019-12-18 09:32:49 +0100

    Fix branch for dev script
    `master` is now the main development branch.
    
    Reference: https://github.com/FreshRSS/FreshRSS/issues/2668

--- scripts/update_to_dev.php
+++ scripts/update_to_dev.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PACKAGE_URL', 'https://codeload.github.com/FreshRSS/FreshRSS/zip/dev');
+define('PACKAGE_URL', 'https://codeload.github.com/FreshRSS/FreshRSS/zip/master');
 
 
 // Apply the update by replacing old version of FreshRSS by the new one.
